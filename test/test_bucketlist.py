743804130667dfd8606563856dc8a6589eaf9e0f
Muhwezi Allan <muhallan1@gmail.com>
2017-08-20 16:43:40 +0300

    added a setUp method that initializes the app and its test client

--- test/test_bucketlist.py
+++ test/test_bucketlist.py
@@ -6,3 +6,17 @@ from app import create_app, db
 
 class BucketlistTestCase(unittest.TestCase):
     """This class represents the bucketlist test case"""
+
+    def setUp(self):
+        """
+        Initialize the app and its test client and our test database
+        :return:
+        """
+        self.app = create_app(config_name="testing")
+        self.client = self.app.test_client
+        self.bucketlist = {'name': 'Go to the Grand canyon for camping'}
+
+        # binds the app to the current context
+        with self.app.app_context():
+            # create all tables
+            db.create_all()
