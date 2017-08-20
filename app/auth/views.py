446d98d7614f84e4109074f35f89ab143f9eac88
Muhwezi Allan <muhallan1@gmail.com>
2017-08-20 21:37:24 +0300

    added a view for registration of a new user

---
+++ app/auth/views.py
@@ -0,0 +1,9 @@
+from . import auth_blueprint
+
+from flask.views import MethodView
+from flask import make_response, request, jsonify
+from app.models import User
+
+
+class RegistrationView(MethodView):
+    """This class registers a new user."""
