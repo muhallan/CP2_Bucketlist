254ddde5fff699ffff99afe3ca3f7d2adc0414db
Muhwezi Allan <muhallan1@gmail.com>
2017-08-20 20:10:52 +0300

    created the setUp method for setting up the test variables and configurations

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -5,3 +5,20 @@ from app import create_app, db
 
 class AuthTestCase(unittest.TestCase):
     """Test case for the authentication blueprint."""
+
+    def setUp(self):
+        """Set up test variables."""
+        self.app = create_app(config_name="testing")
+        # initialize the test client
+        self.client = self.app.test_client
+        # This is the user test json data with a predefined email and password
+        self.user_data = {
+            'email': 'test@example.com',
+            'password': 'test_password'
+        }
+
+        with self.app.app_context():
+            # create all tables
+            db.session.close()
+            db.drop_all()
+            db.create_all()
