5f8d65d8523cabdf33f2f781968d71138cb98bd6
Muhwezi Allan <muhallan1@gmail.com>
2017-08-20 21:48:48 +0300

    added test for login functionality

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -51,3 +51,20 @@ class AuthTestCase(unittest.TestCase):
         result = json.loads(second_res.data.decode())
         self.assertEqual(
             result['message'], "User already exists. Please login.")
+
+    def test_user_login(self):
+        """
+        Test if a user who is registered can login successfully
+        :return:
+        """
+        res = self.client().post('/auth/register', data=self.user_data)
+        self.assertEqual(res.status_code, 201)
+        login_res = self.client().post('/auth/login', data=self.user_data)
+
+        # get the results in json format
+        result = json.loads(login_res.data.decode())
+        # check if the response contains success message
+        self.assertEqual(result['message'], "You logged in successfully.")
+        # Assert that the status code is equal to 200
+        self.assertEqual(login_res.status_code, 200)
+        self.assertTrue(result['access_token'])
