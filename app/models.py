8e5ce425d4ec759b8976299b0225713c7dfcc368
Muhwezi Allan <muhallan1@gmail.com>
2017-08-20 15:23:20 +0300

    added comments to the methods describing their functionalities

--- app/models.py
+++ app/models.py
@@ -14,13 +14,33 @@ class Bucketlist(db.Model):
         onupdate=db.func.current_timestamp())
 
     def __init__(self, name):
-        """initialize with name."""
+        """
+        Init method to initialize a bucketlist
+        :param name:
+        """
         self.name = name
 
     def save(self):
+        """
+        To save a new or edit a bucketlist to the database
+        :return:
+        """
         db.session.add(self)
         db.session.commit()
 
     @staticmethod
     def get_all(self):
+        """
+        To get all the bucketlists that are saved
+        :param self:
+        :return:
+        """
         return Bucketlist.query.all()
+
+    def delete(self):
+        """
+        To delete an existing bucketlist from the database
+        :return:
+        """
+        db.session.delete(self)
+        db.session.commit()
