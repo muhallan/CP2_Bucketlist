501c50cef0c09c2a153c9f4a4ac8849f43244818
Muhwezi Allan <muhallan1@gmail.com>
2017-08-22 18:08:16 +0300

    added a url_prefix of /api/v1 to the authentication blueprint to mark this as version 1 of the api in the url

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
 # This instance of a Blueprint that represents the authentication blueprint
-auth_blueprint = Blueprint('auth', __name__)
+auth_blueprint = Blueprint('auth', __name__, url_prefix='/api/v1') # added v1 to indicate that this is version 1
 
 from . import views
