4eb6ccdf8d0178f602b01e448d55e910427314cf
Muhwezi Allan <muhallan1@gmail.com>
2017-08-22 18:22:34 +0300

    moved comment to above the statement

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 
 # This instance of a Blueprint that represents the authentication blueprint
-auth_blueprint = Blueprint('auth', __name__, url_prefix='/api/v1') # added v1 to indicate that this is version 1
+# added v1 to indicate that this is version 1
+auth_blueprint = Blueprint('auth', __name__, url_prefix='/api/v1')
 
 from . import views
