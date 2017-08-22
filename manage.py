5653cdce1aebee17bd85263a050d54e8f40e8a54
Muhwezi Allan <muhallan1@gmail.com>
2017-08-23 00:13:59 +0300

    commented out os get environment variables for purposes of testing

--- manage.py
+++ manage.py
@@ -7,8 +7,8 @@ from app import db, create_app
 from app import models
 
 # initialize the app with all its configurations
-app = create_app(config_name=os.getenv('APP_SETTINGS'))
-# app = create_app(config_name="development")
+# app = create_app(config_name=os.getenv('APP_SETTINGS'))
+app = create_app(config_name="development")
 migrate = Migrate(app, db)
 # create an instance of class that will handle our commands
 manager = Manager(app)
