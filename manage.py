a09f9433f7ac3bf0718856bd1864438e6ac92a7f
Muhwezi Allan <muhallan1@gmail.com>
2017-08-25 00:34:42 +0300

    added support for coverage

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+import coverage
 # class for handling a set of commands
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
@@ -7,8 +8,8 @@ from app import db, create_app
 from app import models
 
 # initialize the app with all its configurations
-app = create_app(config_name=os.getenv('APP_SETTINGS'))
-# app = create_app(config_name="development")
+# app = create_app(config_name=os.getenv('APP_SETTINGS'))
+app = create_app(config_name="development")
 migrate = Migrate(app, db)
 # create an instance of class that will handle our commands
 manager = Manager(app)
@@ -28,6 +29,33 @@ def test():
     if result.wasSuccessful():
         return 0
     return 1
+
+@manager.command
+def cov():
+    """Runs the unit tests with coverage."""
+    cov = coverage.coverage(branch=True, include='app/*')
+    cov.start()
+    tests = unittest.TestLoader().discover('./tests', pattern='test*.py')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    cov.stop()
+    cov.save()
+    print('Coverage Summary:')
+    cov.report()
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    covdir = os.path.join(basedir, 'tmp/coverage')
+    cov.html_report(directory=covdir)
+    print('HTML version: file://%s/index.html' % covdir)
+    cov.erase()
+
+@manager.command
+def create_db():
+    """Creates the db tables."""
+    db.create_all()
+
+@manager.command
+def drop_db():
+    """Drops the db tables."""
+    db.drop_all()
 
 if __name__ == '__main__':
     manager.run()
