171877743c4d7632d059e490e4bdb00db1a5428f
Muhwezi Allan <muhallan1@gmail.com>
2017-08-24 17:19:19 +0300

    added heroku database uri

--- instance/config.py
+++ instance/config.py
@@ -6,11 +6,11 @@ class Config(object):
     """Parent configuration class."""
     DEBUG = False
     CSRF_ENABLED = True
-    SECRET = os.getenv('SECRET')
-    #SECRET = 'theboythatwentaway'
+    # SECRET = os.getenv('SECRET')
+    SECRET = 'theboythatwentaway'
     # SECRET_KEY = 'theboythatwentaway'
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    # SQLALCHEMY_DATABASE_URI = "postgresql://localhost/bucketlist_api"
+    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = "postgresql://localhost/bucketlist_api"
 
 
 class DevelopmentConfig(Config):
@@ -28,11 +28,13 @@ class TestingConfig(Config):
 class StagingConfig(Config):
     """Configurations for Staging."""
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgres://ruybwyovidomdq:ebc72b1495e5e5f3f3c0badcfd279fdfe17624a4d7387c3d0c0e2ee2233f997f@ec2-107-22-211-182.compute-1.amazonaws.com:5432/d2rrklu5e9n72i'
 
 
 class ProductionConfig(Config):
     """Configurations for Production."""
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'postgres://kkolmsmoopltcf:21ae474621e9d2156f04b547e9b2cfcef584d83eb8b55c663f93d07bbbde40ab@ec2-107-22-211-182.compute-1.amazonaws.com:5432/d9ra0tvddslop0'
     TESTING = False
 
 app_config = {
