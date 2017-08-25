0d49d62795801670ed70f27ea9464665cb24317a
Muhwezi Allan <muhallan1@gmail.com>
2017-08-25 14:58:17 +0300

    removed hardcoded app settings

--- instance/config.py
+++ instance/config.py
@@ -6,8 +6,7 @@ class Config(object):
     """Parent configuration class."""
     DEBUG = False
     CSRF_ENABLED = True
-    # SECRET = os.getenv('SECRET')
-    SECRET = 'theboythatwentaway'
+    SECRET = os.getenv('SECRET')
     # SECRET_KEY = 'theboythatwentaway'
     # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_DATABASE_URI = "postgresql://localhost/bucketlist_api"
