bf7be8b3a568815d595d94a2b2d8595245e509fc
Muhwezi Allan <muhallan1@gmail.com>
2017-08-29 15:05:51 +0300

    included the tearDown method

--- tests/test_bucketlist_item_deletion.py
+++ tests/test_bucketlist_item_deletion.py
@@ -194,3 +194,14 @@ class BucketlistTestCase(unittest.TestCase):
             headers=dict(Authorization=""), )
         self.assertEqual(res.status_code, 401)
         self.assertIn('Token not provided in the header with key Authorization.', str(res.data))
+
+    def tearDown(self):
+        """teardown all initialized variables."""
+        with self.app.app_context():
+            # drop all tables
+            db.session.remove()
+            db.drop_all()
+            db.create_all()
+
+    if __name__ == "__main__":
+        unittest.main()
