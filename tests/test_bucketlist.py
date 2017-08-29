cf094f8000073aab50ee6af2437442353e0a9a46
Muhwezi Allan <muhallan1@gmail.com>
2017-08-29 13:00:49 +0300

    added more tests about bucketlist items manipulation

--- tests/test_bucketlist.py
+++ tests/test_bucketlist.py
@@ -748,6 +748,179 @@ class BucketlistTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 201)
         self.assertIn('Eat fried', str(res.data))
 
+    def test_create_bucketlist_item_with_no_auth_header(self):
+        """
+        Test what message is displayed when no header is provided
+        :return:
+        """
+        self.register_user()
+        result = self.login_user()
+        access_token = json.loads(result.data.decode())['access_token']
+
+        res = self.client().post(
+            '/api/v1/bucketlists/',
+            headers=dict(Authorization="Bearer " + access_token),
+            data={'name': 'Visit the Grand Canyon!'})
+        self.assertEqual(res.status_code, 201)
+        # get the bucketlist in json
+        results = json.loads(res.data.decode())
+
+        # add a new bucketlist item
+        res = self.client().post(
+            '/api/v1/bucketlists/{}/items/'.format(results['id']),
+            headers=dict(), data={
+                "name": "Eat fried crabs"
+            })
+
+        self.assertEqual(res.status_code, 401)
+        self.assertIn('Header with key Authorization missing.', str(res.data))
+
+    def test_create_bucketlist_item_with_invalid_token(self):
+        """
+        Test if a bucketlist is actually created using a resulting status code
+        :return:
+        """
+        self.register_user()
+        result = self.login_user()
+        access_token = json.loads(result.data.decode())['access_token']
+
+        res = self.client().post(
+            '/api/v1/bucketlists/',
+            headers=dict(Authorization="Bearer " + access_token),
+            data=self.bucketlist)
+
+        # assert that the bucketlist is created
+        self.assertEqual(res.status_code, 201)
+        # get the response data in json format
+        results = json.loads(res.data.decode())
+
+        # add a new bucketlist item
+        res = self.client().post(
+            '/api/v1/bucketlists/{}/items/'.format(results['id']),
+            headers=dict(Authorization=access_token), data={
+                "name": "Eat fried crabs"
+            })
+
+        self.assertEqual(res.status_code, 401)
+        self.assertIn('Invalid token format.', str(res.data))
+
+    def test_create_bucketlist_item_with_empty_token(self):
+        """
+        Test what message is displayed when a header is provided with an empty token
+        :return:
+        """
+        self.register_user()
+        result = self.login_user()
+        access_token = json.loads(result.data.decode())['access_token']
+
+        res = self.client().post(
+            '/api/v1/bucketlists/',
+            headers=dict(Authorization="Bearer " + access_token),
+            data=self.bucketlist)
+
+        # assert that the bucketlist is created
+        self.assertEqual(res.status_code, 201)
+        # get the response data in json format
+        results = json.loads(res.data.decode())
+
+        # add a new bucketlist item
+        res = self.client().post(
+            '/api/v1/bucketlists/{}/items/'.format(results['id']),
+            headers=dict(Authorization=""), data={
+                "name": "Eat fried crabs"
+            }
+        )
+        self.assertEqual(res.status_code, 401)
+        self.assertIn('Token not provided in the header with key Authorization.', str(res.data))
+
+    def test_bucketlist_item_creation_with_no_name_parameter(self):
+        """
+        Test what is displayed when a name is provided during bucketlist creation
+        :return:
+        """
+        self.register_user()
+        result = self.login_user()
+        access_token = json.loads(result.data.decode())['access_token']
+
+        res = self.client().post(
+            '/api/v1/bucketlists/',
+            headers=dict(Authorization="Bearer " + access_token),
+            data=self.bucketlist)
+
+        # assert that the bucketlist is created
+        self.assertEqual(res.status_code, 201)
+        # get the response data in json format
+        results = json.loads(res.data.decode())
+
+        # create a bucketlist item by making a POST request
+        res = self.client().post(
+            '/api/v1/bucketlists/{}/items/'.format(results['id']),
+            headers=dict(Authorization="Bearer " + access_token),
+            data=dict())
+        self.assertEqual(res.status_code, 400)
+        self.assertIn('Parameter name missing.', str(res.data))
+
+    def test_bucketlist_item_creation_with_empty_name(self):
+        """
+        Test what is displayed when attempting to creating a bucketlist with an empty name
+        :return:
+        """
+        self.register_user()
+        result = self.login_user()
+        access_token = json.loads(result.data.decode())['access_token']
+
+        res = self.client().post(
+            '/api/v1/bucketlists/',
+            headers=dict(Authorization="Bearer " + access_token),
+            data=self.bucketlist)
+
+        # assert that the bucketlist is created
+        self.assertEqual(res.status_code, 201)
+        # get the response data in json format
+        results = json.loads(res.data.decode())
+
+        # create a bucketlist item by making a POST request
+        res = self.client().post(
+            '/api/v1/bucketlists/{}/items/'.format(results['id']),
+            headers=dict(Authorization="Bearer " + access_token),
+            data={'name': ''})
+        self.assertEqual(res.status_code, 400)
+        self.assertIn('Bucketlist item name should not be empty.', str(res.data))
+
+    def test_bucketlist_item_creation_with_duplicate_name(self):
+        """
+        Test what is displayed when attempting to creating a bucketlist with a name that already exists
+        :return:
+        """
+        self.register_user()
+        result = self.login_user()
+        access_token = json.loads(result.data.decode())['access_token']
+
+        res = self.client().post(
+            '/api/v1/bucketlists/',
+            headers=dict(Authorization="Bearer " + access_token),
+            data=self.bucketlist)
+
+        # assert that the bucketlist is created
+        self.assertEqual(res.status_code, 201)
+        # get the response data in json format
+        results = json.loads(res.data.decode())
+
+        # create a bucketlist item by making a POST request
+        res_item_1 = self.client().post(
+            '/api/v1/bucketlists/{}/items/'.format(results['id']),
+            headers=dict(Authorization="Bearer " + access_token),
+            data={'name': 'sky dive'})
+
+        # create the same bucketlist
+        res_again = self.client().post(
+            '/api/v1/bucketlists/{}/items/'.format(results['id']),
+            headers=dict(Authorization="Bearer " + access_token),
+            data={'name': 'sky dive'})
+
+        self.assertEqual(res_again.status_code, 409)
+        self.assertIn('Bucketlist item with this name already exists in this bucketlist. Choose another name.', str(res_again.data))
+
     def test_update_bucketlist_item(self):
         """Test if a bucketlist item can be edited"""
         self.register_user()
@@ -788,6 +961,242 @@ class BucketlistTestCase(unittest.TestCase):
             '/api/v1/bucketlists/{}/items/{}'.format(results['id'], res_item['id']),
             headers=dict(Authorization="Bearer " + access_token))
         self.assertIn('mixed with honey', str(results.data))
+
+    def test_bucketlist_item_edit_with_no_name_parameter(self):
+        """
+        Test what is displayed when a name is provided during bucketlist creation
+        :return:
+        """
+        self.register_user()
+        result = self.login_user()
+        access_token = json.loads(result.data.decode())['access_token']
+
+        # first, we create a bucketlist by making a POST request
+        res = self.client().post(
+            '/api/v1/bucketlists/',
+            headers=dict(Authorization="Bearer " + access_token),
+            data={'name': 'Visit the Grand canyon'})
+        self.assertEqual(res.status_code, 201)
+        # get the json with the bucketlist
+        results = json.loads(res.data.decode())
+
+        # create a bucketlist item by making a POST request and add it to the created bucketlist
+        res = self.client().post(
+            '/api/v1/bucketlists/{}/items/'.format(results['id']),
+            headers=dict(Authorization="Bearer " + access_token),
+            data={
+                "name": "Eat fried crabs"
+            })
+        self.assertEqual(res.status_code, 201)
+        # get the json containing the created bucketlist item
+        res_item = json.loads(res.data.decode())
+
+        # then, we edit the created bucketlist by making a PUT request
+        rv = self.client().put(
+            '/api/v1/bucketlists/{}/items/{}'.format(results['id'], res_item['id']),
+            headers=dict(Authorization="Bearer " + access_token),
+            data=dict())
+        self.assertEqual(res.status_code, 400)
+        self.assertIn('Parameter name missing.', str(res.data))
+
+    def test_bucketlist_item_edit_with_empty_name(self):
+        """
+        Test what is displayed when attempting to creating a bucketlist with an empty name
+        :return:
+        """
+        self.register_user()
+        result = self.login_user()
+        access_token = json.loads(result.data.decode())['access_token']
+
+        # first, we create a bucketlist by making a POST request
+        res = self.client().post(
+            '/api/v1/bucketlists/',
+            headers=dict(Authorization="Bearer " + access_token),
+            data={'name': 'Visit the Grand canyon'})
+        self.assertEqual(res.status_code, 201)
+        # get the json with the bucketlist
+        results = json.loads(res.data.decode())
+
+        # create a bucketlist item by making a POST request and add it to the created bucketlist
+        res = self.client().post(
+            '/api/v1/bucketlists/{}/items/'.format(results['id']),
+            headers=dict(Authorization="Bearer " + access_token),
+            data={
+                "name": "Eat fried crabs"
+            })
+        self.assertEqual(res.status_code, 201)
+        # get the json containing the created bucketlist item
+        res_item = json.loads(res.data.decode())
+
+        # then, we edit the created bucketlist by making a PUT request
+        rv = self.client().put(
+            '/api/v1/bucketlists/{}/items/{}'.format(results['id'], res_item['id']),
+            headers=dict(Authorization="Bearer " + access_token),
+            data={'name': ''})
+        self.assertEqual(rv.status_code, 400)
+        self.assertIn('Bucketlist item name should not be empty.', str(rv.data))
+
+    def test_bucketlist_item_edit_with_duplicate_name(self):
+        """
+        Test what is displayed when attempting to creating a bucketlist with a name that already exists
+        :return:
+        """
+        self.register_user()
+        result = self.login_user()
+        access_token = json.loads(result.data.decode())['access_token']
+
+        # first, we create a bucketlist by making a POST request
+        res = self.client().post(
+            '/api/v1/bucketlists/',
+            headers=dict(Authorization="Bearer " + access_token),
+            data={'name': 'Visit the Grand canyon'})
+        self.assertEqual(res.status_code, 201)
+        # get the json with the bucketlist
+        results = json.loads(res.data.decode())
+
+        # create a bucketlist item by making a POST request and add it to the created bucketlist
+        res = self.client().post(
+            '/api/v1/bucketlists/{}/items/'.format(results['id']),
+            headers=dict(Authorization="Bearer " + access_token),
+            data={
+                "name": "Eat fried crabs"
+            })
+        self.assertEqual(res.status_code, 201)
+        # get the json containing the created bucketlist item
+        res_item = json.loads(res.data.decode())
+
+        # then we create a second bucketlist item by
+        # making a POST request and add it to the created bucketlist
+        res2 = self.client().post(
+            '/api/v1/bucketlists/{}/items/'.format(results['id']),
+            headers=dict(Authorization="Bearer " + access_token),
+            data={
+                "name": "Eat chicken wings"
+            })
+        self.assertEqual(res2.status_code, 201)
+        # get the json containing the created bucketlist item
+        res_item1 = json.loads(res2.data.decode())
+
+        # then, we edit the last created bucketlist item by making a PUT request and giving it the same name
+        res1 = self.client().put(
+            '/api/v1/bucketlists/{}/items/{}'.format(results['id'], res_item1['id']),
+            headers=dict(Authorization="Bearer " + access_token),
+            data={'name': 'Eat fried crabs'})
+        self.assertEqual(res1.status_code, 409)
+        self.assertIn('Bucketlist item with this name already exists in this bucketlist. '
+                      'Choose another name.', str(res1.data))
+
+    def test_single_bucketlist_item_edit_with_no_auth_header(self):
+        """
+        Test what message is displayed when no header is provided
+        :return:
+        """
+        self.register_user()
+        result = self.login_user()
+        access_token = json.loads(result.data.decode())['access_token']
+
+        res = self.client().post(
+            '/api/v1/bucketlists/',
+            headers=dict(Authorization="Bearer " + access_token),
+            data=self.bucketlist)
+
+        # assert that the bucketlist is created
+        self.assertEqual(res.status_code, 201)
+        # get the response data in json format
+        results = json.loads(res.data.decode())
+
+        # create a bucketlist item by making a POST request and add it to the created bucketlist
+        res = self.client().post(
+            '/api/v1/bucketlists/{}/items/'.format(results['id']),
+            headers=dict(Authorization="Bearer " + access_token),
+            data={
+                "name": "Eat fried crabs"
+            })
+        self.assertEqual(res.status_code, 201)
+        # get the json containing the created bucketlist item
+        res_item = json.loads(res.data.decode())
+
+        # edit the added bucketlist item by making a PUT request
+        res = self.client().put(
+            '/api/v1/bucketlists/{}/items/{}'.format(results['id'], res_item['id']),
+            headers=dict(),
+            data={
+                "name": "Must visit the Grand Canyon!"
+            }
+        )
+        self.assertEqual(res.status_code, 401)
+        self.assertIn('Header with key Authorization missing.', str(res.data))
+
+    def test_single_bucketlist_item_edit_with_invalid_token(self):
+        """
+        Test if a bucketlist item is edited when an invalid token is provided and the message provided
+        :return:
+        """
+        self.register_user()
+        result = self.login_user()
+        access_token = json.loads(result.data.decode())['access_token']
+
+        res = self.client().post(
+            '/api/v1/bucketlists/',
+            headers=dict(Authorization="Bearer " + access_token),
+            data=self.bucketlist)
+
+        # assert that the bucketlist is created
+        self.assertEqual(res.status_code, 201)
+        # get the response data in json format
+        results = json.loads(res.data.decode())
+
+        # create a bucketlist item by making a POST request and add it to the created bucketlist
+        res = self.client().post(
+            '/api/v1/bucketlists/{}/items/'.format(results['id']),
+            headers=dict(Authorization="Bearer " + access_token),
+            data={
+                "name": "Eat fried crabs"
+            })
+        self.assertEqual(res.status_code, 201)
+        # get the json containing the created bucketlist item
+        res_item = json.loads(res.data.decode())
+
+        # edit the added bucketlist item by making a PUT request
+        res = self.client().put(
+            '/api/v1/bucketlists/{}/items/{}'.format(results['id'], res_item['id']),
+            headers=dict(Authorization=access_token),
+            data={
+                "name": "Must visit the Grand Canyon!"
+            }
+        )
+        self.assertEqual(res.status_code, 401)
+        self.assertIn('Invalid token format.', str(res.data))
+
+    def test_single_bucketlist_item_edit_with_empty_token(self):
+        """
+        Test what message is displayed when a header is provided with an empty token
+        :return:
+        """
+        self.register_user()
+        result = self.login_user()
+        access_token = json.loads(result.data.decode())['access_token']
+
+        res = self.client().post(
+            '/api/v1/bucketlists/',
+            headers=dict(Authorization="Bearer " + access_token),
+            data=self.bucketlist)
+
+        # assert that the bucketlist is created
+        self.assertEqual(res.status_code, 201)
+        # get the response data in json format
+        results = json.loads(res.data.decode())
+
+        # edit a specific bucketlist by making a PUT request
+        res = self.client().put(
+            '/api/v1/bucketlists/{}'.format(results['id']),
+            headers=dict(Authorization=""),
+            data={
+                "name": "Must visit the Grand Canyon!"
+            }
+        )
+        self.assertEqual(res.status_code, 401)
+        self.assertIn('Token not provided in the header with key Authorization.', str(res.data))
 
     def test_delete_bucketlist_item(self):
         """Test if an item in a bucketlist can be deleted"""
