472becccbc9c45531127f1a08ad27c1f699cc637
Muhwezi Allan <allan.muhwezi@andela.com>
2017-09-06 23:47:38 +0300

    added an authorization decorator to check for the validity of the authorization header

---
+++ app/decorators.py
@@ -0,0 +1,59 @@
+from flask import request, jsonify, abort, make_response
+from app.models import User
+from functools import wraps
+
+
+def authorization(func):
+    @wraps(func)
+    def check_authorization(*args, **kwargs):
+        # check if the header with key is present
+        if 'Authorization' not in request.headers:
+            # Return a message to the user telling them that they need to submit an authorization header with token
+            response = {
+                'message': 'Header with key Authorization missing.'
+            }
+            return make_response(jsonify(response)), 401
+
+        # Get the access token from the header
+        auth_header = request.headers.get('Authorization').strip()
+
+        # check for when authorization was not provided in header
+        if not auth_header:
+            # Return a message to the user telling them that they need to submit an authorization header with token
+            response = {
+                'message': 'Token not provided in the header with key Authorization.'
+            }
+            return make_response(jsonify(response)), 401
+
+        auth_strings = auth_header.split(" ")
+        if len(auth_strings) != 2:
+            response = {
+                'message': 'Invalid token format.'
+            }
+            return make_response(jsonify(response)), 401
+
+        access_token = auth_header.split(" ")[1]
+
+        if access_token:
+            # Attempt to decode the token and get the User ID
+            user_id = User.decode_token(access_token)
+
+            if not isinstance(user_id, str):
+                kwargs['user_id'] = user_id
+
+                return func(*args, **kwargs)
+
+            # user is not legit, so the payload is an error message
+            message = user_id
+            response = {
+                'message': message
+            }
+            return make_response(jsonify(response)), 401
+        else:
+            response = {
+                'message': 'Empty token string'
+            }
+            return make_response(jsonify(response)), 401
+
+    check_authorization.__doc__ = func.__doc__
+    return check_authorization
